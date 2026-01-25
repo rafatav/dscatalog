a566e0514b2d1d15cbabd63ce8ec978d72ba944d
Rafael Tavares <tavares.rfl@gmail.com>
2026-01-25 20:09:44 -0300

    feat: add update product

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -33,10 +33,21 @@ public class ProductService {
     @Transactional
     public ProductDTO insert(ProductDTO dto) {
         Product product = new Product();
+        copyDtoToEntity(dto, product);
+        return new ProductDTO(repository.save(product));
+    }
+
+    @Transactional
+    public ProductDTO update(ProductDTO dto, Long id) {
+        Product product = repository.getReferenceById(id);
+        copyDtoToEntity(dto, product);
+        return new ProductDTO(repository.save(product));
+    }
+
+    private void copyDtoToEntity(ProductDTO dto, Product product) {
         product.setName(dto.getName());
         product.setDescription(dto.getDescription());
         product.setPrice(dto.getPrice());
         product.setImgUrl(dto.getImgUrl());
-        return new ProductDTO(repository.save(product));
     }
 }
