0c291c6138bca487bdb0e978c09b1f437e162840
Rafael Tavares <tavares.rfl@gmail.com>
2026-01-25 21:11:50 -0300

    feat: add user and role entities

---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Role.java
@@ -0,0 +1,45 @@
+package com.devsuperior.dscatalog.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "tb_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String authority;
+
+//    private List<User> users = new ArrayList<>();
+
+    public Role() {
+    }
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -5,3 +5,9 @@ INSERT INTO tb_category (name) VALUES ('Móveis')
 INSERT INTO tb_product (name, description, price, img_url) VALUES ('TV Samsung', 'TV de Led de 55 polegadas', 3999.99, 'tv.jpeg')
 INSERT INTO tb_product (name, description, price, img_url) VALUES ('Playstation 5', 'Videogame da Sony', 3230, 'ps5.jpeg')
 INSERT INTO tb_product (name, description, price, img_url) VALUES ('iPhone 17', 'Celular da Apple', 10599.99, 'iphone.jpeg')
+
+INSERT INTO tb_role (authority) VALUES ('Cliente')
+INSERT INTO tb_role (authority) VALUES ('Administrador')
+
+INSERT INTO tb_user (first_name, last_name, email, password) VALUES ('Rafael', 'Tavares', 'tavares@gmail.com', '123456')
+INSERT INTO tb_user (first_name, last_name, email, password) VALUES ('Tom', 'Cruise', 'tom@gmail.com', '1234567')
